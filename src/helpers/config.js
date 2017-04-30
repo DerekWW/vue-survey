8a27154c9ff3168edb9347a490ed1421d89e75c1
Derek <derek.wierson@gmail.com>
2017-04-30 12:56:45 -0700

    Delete config.js

--- src/helpers/config.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default {
-  clientId: '1018029921808-gr8uu4k8m6aup9h0un2t7d5cv6vm749n.apps.googleusercontent.com',
-  apiKey: 'AIzaSyDVvtSK4fZ6htviqyvwUsMAQ-MYbPkgCEk',
-  spreadsheetId: '1tA60Gczx8N2451Ul9_0nPfoMTggHa6k8JWoJQdoJvOI'
-}
