4bc4b34ebf6d30a1a2f7f314ed804c64fdeaeb22
DerekWW <derek.wierson@gmail.com>
2017-06-21 10:42:22 -0700

    create and populate select box with student names from data

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,9 +1,10 @@
 <template lang="html">
  <div class="row container">
    <div class="input-field col s12">
-     <select>
+     <select class="browser-default" v-model="selectedStudent">
        <option value="" disabled selected>Choose your student</option>
-       <option value="1">Option 1</option>
+       <option v-for="studentname in studentNameList" v-bind:value="studentname">{{studentname}}</option>
+
      </select>
    </div>
  </div>
@@ -11,17 +12,35 @@
 
 <script>
 /* eslint-disable */
+import _ from 'lodash';
+
 export default {
   name: 'Student',
   data: function() {
     return {
       students: {},
-      recent: this.orderedByDay.slice(0, 25)
+      recent: this.orderedByDay.slice(0, 25),
+      selectedStudent: '',
+      studentNameList: [],
+      currentStudent: null
     }
   },
   props: ['rows', 'orderedByDay'],
   beforeMount: function() {
+    this.recent.forEach((day) => {
 
+      day.students.forEach((student) => {
+
+        if (!this.students[student[1]]) {
+          this.students[student[1]] = []
+          this.studentNameList.push(student[1])
+        }
+
+
+
+      })
+
+    })
   },
   created: function() {
     $(document).ready(function() {
